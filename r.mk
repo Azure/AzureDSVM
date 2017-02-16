4fccf7874772b7a4e5db2b535ff9b40218f82a81
Graham Williams <graham.williams@togaware.com>
2017-02-16 11:45:19 +0800

    Add a run target to push vignette r scripts through.

--- r.mk
+++ r.mk
@@ -4,6 +4,10 @@ RSCRIPT_OPTS = --vanilla
 %.R: %.Rmd
 	${RSCRIPT} ${RSCRIPT_OPTS} -e 'library(knitr);purl("$<", out="$@")'	
 
+%.run: %.R
+	cd $(shell dirname "$<"); ${RSCRIPT} ${RSCRIPT_OPTS} -e 'source("$(shell basename $<)")'	
+
+
 check: 
 	R CMD check --check-subdirs=yes .
 
