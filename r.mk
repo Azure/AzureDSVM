68aad920a31806e5947680c0f119ed6ff0fa55d5
Graham Williams <graham.williams@togaware.com>
2017-02-28 21:08:04 +0800

    Update with standard R operations.

--- r.mk
+++ r.mk
@@ -1,8 +1,17 @@
+PKG_VERSION = $(shell grep -i ^version DESCRIPTION | cut -d : -d \  -f 2)
+PKG_NAME    = $(shell grep -i ^package DESCRIPTION | cut -d : -d \  -f 2)
+
 RSCRIPT      = Rscript
 RSCRIPT_OPTS = --vanilla
 
-VRMD = $(wildcard vignettes/*.Rmd)
-VR   = $(VRMD:.Rmd=.R)
+VIGNETTE_RMD = $(wildcard vignettes/*.Rmd)
+VIGNETTE_R   = $(VRMD:.Rmd=.R)
+
+R_FILES   := $(wildcard R/*.R)
+SRC_FILES := $(wildcard src/*) $(addprefix src/, $(COPY_SRC))
+PKG_FILES := DESCRIPTION NAMESPACE $(R_FILES) $(SRC_FILES)
+
+.PHONY: list doc vignettes tarball check install build clean realclean
 
 %.R: %.Rmd
 	${RSCRIPT} ${RSCRIPT_OPTS} -e 'library(knitr);purl("$<", out="$@")'	
@@ -11,23 +20,39 @@ VR   = $(VRMD:.Rmd=.R)
 	cd $(shell dirname "$<");\
 	${RSCRIPT} ${RSCRIPT_OPTS} -e 'source("$(shell basename $<)")'	
 
-check: 
-	R CMD check --check-subdirs=yes .
+$(PKG_NAME)_$(PKG_VERSION).tar.gz: $(PKG_FILES)
+	R CMD build .
 
-.PHONY: doc
+list:
+	@echo -e "PKG_NAME:    $(PKG_NAME)"
+	@echo -e "PKG_VERSION: $(PKG_VERSION)"
+	@echo -e "R_FILES:     $(R_FILES)"
+	@echo -e "SRC_FILES:   $(SRC_FILES)"
+
 doc:
 	R -e 'devtools::document()'
 
-build: doc
-	R CMD build .
+vignettes: $(VIGNETTE_R)
 
-install: build
-	R CMD INSTALL $(PKG)_$(VER).tar.gz
+tarball: doc $(PKG_NAME)_$(PKG_VERSION).tar.gz
 
-.PHONY: vignettes
-vignettes: $(VR)
+check: $(PKG_NAME)_$(PKG_VERSION).tar.gz
+	R CMD check $(PKG_NAME)_$(PKG_VERSION).tar.gz
 
-# Cleanup
+build: $(PKG_NAME)_$(PKG_VERSION).tar.gz
+	R CMD INSTALL -build $(PKG_NAME)_$(PKG_VERSION).tar.gz
+
+install: $(PKG_NAME)_$(PKG_VERSION).tar.gz
+	R CMD INSTALL $(PKG_NAME)_$(PKG_VERSION).tar.gz
+
+NAMESPACE: $(R_FILES)
+	Rscript -e "library(roxygen2);roxygenize('.')"
+
+clean:
+	-rm -f $(PKG_NAME)_*.tar.gz
+	-rm -r -f $(PKG_NAME).Rcheck
+	-rm -r -f man/*
+	-rm -r -f NAMESPACE
 
 realclean:
-	rm -f $(VR)
+	rm -f $(VIGNETTE_R)
