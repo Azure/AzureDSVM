93ac6de142e96cb573bfeaf8179ffa7d3b3f3def
Graham Williams <graham.williams@togaware.com>
2017-03-03 09:23:01 +0800

    Delete all resource groups matching the pattern.

--- test/deleteRG.R
+++ test/deleteRG.R
@@ -1,20 +1,30 @@
-# Delete a Resource Group
+# Delete all my_* Resource Groups
 #
 # 2017-02-27 09:10:09 Graham Williams
 
+library(stringr)
 source("common.R")
+
+# List the resource groups.
+
+context %>%
+  azureListRG() %>%
+  extract2("name") %>%
+  grep(pattern="^my_", value=TRUE) %T>%
+  print() ->
+rgs
 
 # Check if the resource group already exists. Take note this script
 # will not remove the resource group if it pre-existed.
 
-rg_pre_exists <- existsRG(context, RG, LOC) %T>% print()
-
-if (rg_pre_exists)
+for (r in rgs)
 {
-  # Delete the resource group RG.
-
   # Note that to delete a resource group can take some time, like 10 minutes.
 
-  azureDeleteResourceGroup(context, RG)
+  cat(r, "\n")
 
+  if (existsRG(context, r, LOC))
+  {
+    azureDeleteResourceGroup(context, r)
+  }
 }
