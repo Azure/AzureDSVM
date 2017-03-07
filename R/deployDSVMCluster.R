3dbdada15160eba3eec029bf5578faff433f0396
yueguoguo <yueguoguo1024@gmail.com>
2017-03-07 14:40:51 +0800

    Change wd to /temp to avoid drive letter that causes scp issues for Windows local machine

--- R/deployDSVMCluster.R
+++ R/deployDSVMCluster.R
@@ -168,16 +168,12 @@ deployDSVMCluster <- function(context,
     # tmpkeys   <- paste0("./AzureDSR_pubkeys_", hostnames[i], "_")
     file.create(tmpkeys)
 
-    # scp is for Linux and pscp is for Windows.
-    sys <- Sys.info()
-    if(sys[1]) == "Windows") {
-      # NOTE this requires installation of PuTTy on Windows local machine.
-      copy <- "pscp"
-    } else if (sys[1] == "Linux") {
-      copy <- "scp"
-    } else {
-      stop("Operation system not recognized.")
-    }
+    # set working directory to where temp files are located.
+    current_wd <- getwd()
+    setwd(sub(tmpkeys, pattern=basename(tmpkeys), replacement=""))
+
+    copy <- "scp"
+    options <- "-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
     for (i in 1:count)
     {
@@ -186,12 +182,9 @@ deployDSVMCluster <- function(context,
       # of avoiding pop up. Also do not clog up the user's known_hosts
       # file with all the servers created.
 
-      options <- "-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-
       tmpkey <- tempfile(paste0("AzureDSR_pubkey_", hostnames[i], "_"))
-      file.create(tmpkey)
-
       # tmpkey <- paste0("./AzureDSR_pubkey_", hostnames[i], "_")
+      file.create(tmpkey)
 
       # Generate key pairs in the VM
 
@@ -203,7 +196,8 @@ deployDSVMCluster <- function(context,
       # Copy the public key and append it to the local machine.
 
       cmd <- sprintf("%s %s %s@%s:.ssh/id_rsa.pub %s",
-                     copy, options, usernames[i], fqdns[i], tmpkey)
+                     # copy, options, usernames[i], fqdns[i], tmpkey)
+                     copy, options, usernames[i], fqdns[i], file.path(".", basename(tmpkey)))
 
       system(cmd)
 
@@ -221,8 +215,8 @@ deployDSVMCluster <- function(context,
     # Create a config file. To avoid any prompt when nodes are
     # communicating with each other.
 
-    tmpscript <- tempfile(paste0("AzureDSR_script_", hostnames[i], "_"))
-    # tmpscript <- paste0("./AzureDSR_script_", hostnames[i], "_")
+    # tmpscript <- tempfile(paste0("AzureDSR_script_", hostnames[i], "_"))
+    tmpscript <- paste0("./AzureDSR_script_", hostnames[i], "_")
     file.create(tmpscript)
 
     sh <- writeChar(paste0("cat .ssh/pub_keys >> .ssh/authorized_keys\n",
@@ -240,12 +234,14 @@ deployDSVMCluster <- function(context,
       # Copy the pub_keys onto node.
 
       system(sprintf("%s %s %s %s@%s:.ssh/pub_keys",
-                     copy, options, tmpkeys, usernames[i], fqdns[i]))
+                     # copy, options, tmpkeys, usernames[i], fqdns[i]))
+                     copy, options, file.path(".", basename(tmpkeys)), usernames[i], fqdns[i]))
 
       # Copy the config onto node and run it.
 
       system(sprintf("%s %s %s %s@%s:.ssh/shell_script",
-                     copy, options, tmpscript, usernames[i], fqdns[i]))
+                     # copy, options, tmpscript, usernames[i], fqdns[i]))
+                     copy, options, file.path(".", basename(tmpscript)), usernames[i], fqdns[i]))
 
       system(sprintf("ssh %s -l %s %s 'chmod +x .ssh/shell_script'",
                      options, usernames[i], fqdns[i]))
@@ -258,6 +254,10 @@ deployDSVMCluster <- function(context,
 
     file.remove(tmpkeys, tmpscript)
   }
+
+  # revert to working directory.
+
+  setwd(current_wd)
 
   # Return results for reference.
 
