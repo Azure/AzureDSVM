42723072b877bc942c1ce2bc994aea0cdea40653
yueguoguo <yueguoguo1024@gmail.com>
2017-03-30 17:36:48 +0800

    Rename

--- test/workerCluster.R
+++ test/workerCluster.R
@@ -21,25 +21,25 @@ if (rxGetComputeContext()@description == "dopar") {
 # make a function to do clustering of given data set.
 
 clusterAnalysis <- function(data, numClusters) {
-
+  
   xdf <- RxXdfData(data)
-
+  
   # create formula.
-
+  
   names <- rxGetVarNames(data=xdf)
   names <- names[!(names %in% c("Class", "Time"))] # the original data set is labelled so remove the label.
   formula <- as.formula(paste0("~", paste(names, collapse="+")))
-
+  
   # to scale data.
-
+  
   df <- rxImport(xdf,
                  varsToDrop=c("Time", "Class"))
   df <- as.data.frame(scale(df))
-
+  
   clusters <- rxKmeans(formula,
                        df,
                        numClusters=numClusters)
-
+  
   clusters$cluster
 }
 
