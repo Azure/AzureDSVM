81fef4822f21cf5e8e81ac7c32ef07ca628f3315
yueguoguo <yueguoguo1024@gmail.com>
2017-06-14 12:25:15 +0800

    Import test and assert packages

--- R/azureDSVM-package.R
+++ R/azureDSVM-package.R
@@ -9,6 +9,8 @@
 #'
 #' @import dplyr
 #' @import magrittr
+#' @import assertthat
+#' @import testthat
 #' @importFrom jsonlite fromJSON
 #' @importFrom httr add_headers headers content status_code http_status authenticate
 #' @importFrom httr GET PUT POST
