447e7e609447f5fd946544177e71c16c135004b9
yueguoguo <yueguoguo1024@gmail.com>
2017-06-16 16:25:18 +0800

    Removed redundant comments.

--- R/deployDSVM.R
+++ R/deployDSVM.R
@@ -41,7 +41,7 @@
 #' Usually an error message will be returned if the deployment is unsuccessful. If the deployment fails without explicit error message returned visit the Azure portal
 #' https://ms.portal.azure.com and browse to the resource group and
 #' click on the failed deployment link to view the failure
-#' message. Typical errors include DnsRecordInUse or
+#' message. Typical errors include DnsRecordInUsehha or
 #' StorageAccountAlreadyTaken. If so then choose a different hostname.
 #'
 #' @export
@@ -119,22 +119,6 @@ deployDSVM <- function(context,
 
   if(!(size %in% unlist(vm_available[, 1])))
     stop("Unknown size - see getVMSizes() for allowed options.")
-
-  # # Incorrect naming of a vm may lead to an unsuccessful deployment of
-  # # the DSVM - normally it returns a 400 error from REST call. Check
-  # # the name here to ensure it is valid.
-  # 
-  # if(length(hostname) > 15)
-  #   stop("Name of virtual machine is too long at ", length(hostname),
-  #        "characters. At most it can be 15 characters.")
-  # 
-  # if(grepl("[[:upper:]]|[[:punct:]]", hostname))
-  #   stop("Name of virtual machine is not valid - only lowercase and digits permitted.")
-
-  # check if password is valid.
-
-  # if(!grepl("^(?=.*[[A-Za-z]])(?=.*\\d)(?=.*[[$@$!%*#?&]])[[A-Za-z\\d$@$!%*#?&]]{8,}$", password))
-  #   stop("Password not valid - minimum 8 characters with at least one digit and one special character.")
 
   # Load template and parameter JSON files for deployment
 
