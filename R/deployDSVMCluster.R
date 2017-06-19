bb6e3dcb747b0aa1b4581b60668d66f6f8adfe6f
yueguoguo <yueguoguo1024@gmail.com>
2017-06-19 13:47:05 +0800

    Arguments inherited from deployDSVM

--- R/deployDSVMCluster.R
+++ R/deployDSVMCluster.R
@@ -1,39 +1,17 @@
 #' Deploy a cluster of Linux Data Science Virtual Machines on Azure.
 #'
 #' Creates a cluster of Data Science Virtual Machines and enables the
-#' DSVMs to communicate across the cluster via public key based
+#' DSVMs to communicate acros the cluster via public key based
 #' credentials for high performance computation. All DSVMs in the
 #' cluster are based on Linux OS and use public key cryptogrphy for
 #' log in.
 #'
 #' @inheritParams deployDSVM
-#'
-#' @param hostnames Hostnames for the DSVMs. Lowercase characters or
-#'   numbers only. If a single hostname is supplied and count > 1 then
-#'   the hostname will be used as a prefix for a sequential count of
-#'   hostnames.
-#'
-#' @param usernames Usernames for the admin account created on the
-#'   DSVM. If a single username is supplied then that username is used
-#'   as the admin user on each host. Otherwise a username is provided
-#'   for each of the DSVMs.
-#'
-#' @param pubkeys Public keys for the DSVM. This is only applicable
-#'   for public-key based authentication of Linux based DSVM. One or a
-#'   vector of public keys can be provided, depending on the count or
-#'   the number of hostnames or usernames.
-#'
+#'   
 #' @param count If provided this is the number of DSVM instances to be
 #'   created. If not provided the number of DSVMs created will be
-#'   either the number of names provided or the number of usernames
+#'   either the number of names provided or the number of username
 #'   provided.
-#'
-#' @param sizes The size of the DSVMs. Each DSVM is the same size.
-#'
-#' @param dns.labels DNS labels for the VM. By default this is the
-#'   hostnames but is not required to be. The fully qualified domain
-#'   name for accessing the deployed DSVM will then be
-#'   "<dns.label_label>.<location>.cloudapp.azure.com".
 #'
 #' @details
 #'
@@ -52,29 +30,29 @@
 #' # The following deploys a cluster of 5 Linux DSVMs.
 #' 
 #' deployDSVMCluster(context, resource.group="<resource_group>", 
-#' location="<location>", hostnames="<machine_name>", usernames="<user_name>", 
-#' os="Windows", pubkeys="<a_valid_public_key_string_in_SSH_format>", count=5)
+#' location="<location>", hostname="<machine_name>", username="<user_name>", 
+#' os="Windows", pubkey="<a_valid_public_key_string_in_SSH_format>", count=5)
 #' 
 #' # The following deploys a collection of 3 Linux DSVMs with different 
 #' names and public keys.
 #' 
 #' deployDSVMCluster(context, resource.group="<resource_group>", 
-#' location="<location>", hostnames=c("<machine_name_1>", "<machine_name_2>", 
-#' "<machine_name_3>", usernames=c("<user_name_1>", "<user_name_2>", 
+#' location="<location>", hostname=c("<machine_name_1>", "<machine_name_2>", 
+#' "<machine_name_3>", username=c("<user_name_1>", "<user_name_2>", 
 #' "<user_name_3>"), os="Windows", 
-#' pubkeys="<a_valid_public_key_string_in_SSH_format>"}
+#' pubkey="<a_valid_public_key_string_in_SSH_format>"}
 deployDSVMCluster <- function(context,
                               resource.group,
                               location,
-                              hostnames,
-                              usernames,
-                              authens="Key",
-                              pubkeys,
-                              passwords,
-                              count,
-                              oss="Ubuntu",
-                              sizes="Standard_D1_v2",
-                              dns.labels=hostnames)
+                              hostname,
+                              username,
+                              authen="Key",
+                              pubkey="",
+                              password="",
+                              os="Ubuntu",
+                              size="Standard_D1_v2",
+                              dns.label=hostname,
+                              count)
 {
 
   # Check if token is valid.
@@ -95,73 +73,73 @@ deployDSVMCluster <- function(context,
     stop("Please specify a data centre location.")
   assert_that(AzureSMR:::is_location(location))
 
-  if(missing(hostnames))
+  if(missing(hostname))
     stop("Please specify virtual machine hostname(s).")
-  assert_that(AzureSMR:::is_vm_name(hostnames))
+  assert_that(AzureSMR:::is_vm_name(hostname))
 
-  if(missing(usernames))
+  if(missing(username))
     stop("Please specify virtual machine username(s).")
-  assert_that(AzureSMR:::is_admin_user(usernames))
+  assert_that(AzureSMR:::is_admin_user(username))
   
-  if(missing(authens))
+  if(missing(authen))
     stop("Please specify authentication method(s).")
   
-  if(authen == "Password" && missing(passwords))
+  if(authen == "Password" && missing(password))
     stop("Please specify virtual machine password(s).")
   
-  if(authen == "Key" && missing(pubkeys))
+  if(authen == "Key" && missing(pubkey))
     stop("Please specify virtual machine public key(s).")
   
-  # length of hostnames, usernames, pubkeys, oss, sizes, and dns labels
+  # length of hostname, username, pubkey, os, size, and dns.label
   # should always be the same.
   
-  input_args_number <- c(length(hostnames),
-                         length(usernames),
-                         length(dns.labels))
+  input_args_number <- c(length(hostname),
+                         length(username),
+                         length(dns.label))
   
   if (!identical(input_args_number, 
                  rep(input_args_number[1], length(input_args_number))))
-    stop("Input host names, user names, and DNS labels 
+    stop("Input host names, user names, and dns.label 
          should all have the same length.")
 
-  # If no count is provided then set it to the number of hostnames or
-  # usernames supplied. 
+  # If no count is provided then set it to the number of hostname or
+  # username supplied. 
 
   if (missing(count))
-    count <- ifelse(length(hostnames) == 1,
-                    ifelse(length(usernames) == 1,
-                           1, length(usernames)),
-                    length(hostnames))
+    count <- ifelse(length(hostname) == 1,
+                    ifelse(length(username) == 1,
+                           1, length(username)),
+                    length(hostname))
   
   if (count == 1) 
     stop("If 'count' is specified, it should be greater than 1.")
 
   # If the count is greater than 1 then ensure we have the right
-  # lengths of hostnames, usernames, and public keys.
+  # lengths of hostname, username, and public keys.
 
-  if (length(hostnames) == 1)
-    hostnames <- sprintf("%s%03d", hostnames, 1:count)
+  if (length(hostname) == 1)
+    hostname <- sprintf("%s%03d", hostname, 1:count)
   
-  if (length(usernames) == 1)
-    usernames <- rep(usernames, count)
+  if (length(username) == 1)
+    username <- rep(username, count)
   
-  if (length(authens) == 1)
-    authens <- rep(authens, count)
+  if (length(authen) == 1)
+    authen <- rep(authen, count)
   
-  if (length(pubkeys) == 1)
-    pubkeys <- rep(pubkeys, count)
+  if (length(pubkey) == 1)
+    pubkey <- rep(pubkey, count)
   
-  if (length(passwords) == 1)
-    passwords <- rep(passwords, count)
+  if (length(password) == 1)
+    password <- rep(password, count)
   
-  if (length(oss) == 1)
-    oss <- rep(oss, count)
+  if (length(os) == 1)
+    os <- rep(os, count)
   
-  if (length(sizes) == 1)
-    sizes <- rep(sizes, count)
+  if (length(size) == 1)
+    size <- rep(size, count)
   
-  if (length(dns.labels) == 1)
-    dns.labels <- rep(dns.labels, count)
+  if (length(dns.label) == 1)
+    dns.label <- rep(dns.label, count)
 
   # Check the number of DSVM deployments to be a reasonable number but
   # allow the user to override. This is only useful if interactive.
@@ -185,13 +163,14 @@ deployDSVMCluster <- function(context,
       deployDSVM(context=context,
                  resource.group=resource.group,
                  location=location,
-                 hostname=hostnames[i],
-                 username=usernames[i],
+                 hostname=hostname[i],
+                 username=username[i],
                  size=size[i],
-                 os=oss[i],
-                 authen=authens[i],
-                 pubkey=pubkeys[i],
-                 dns.label=hostnames[i],
+                 os=os[i],
+                 authen=authen[i],
+                 pubkey=pubkey[i],
+                 password=password[i],
+                 dns.label=hostname[i],
                  mode=ifelse(i == count, "Sync", "ASync"))
   }
 
@@ -203,17 +182,17 @@ deployDSVMCluster <- function(context,
   # NOTE: this will be soon deprecated. The users are encouraged to use
   # doAzureParallel package for high-performance computation.
 
-  if (length(unique(usernames)) == 1)
+  if (length(unique(username)) == 1)
   {
     # sleep for a while as ssh to a ubuntu LDSVM cannot be immediately 
-    executed after deployment.
+    # executed after deployment.
     Sys.sleep(20)
     
     df <- keyDistribution(location=location,
-                          hostnames=hostnames,
-                          usernames=usernames,
+                          hostname=hostname,
+                          username=username,
                           count=count,
-                          dns.labels=dns.labels)
+                          dns.label=dns.label)
     
     df
   }
