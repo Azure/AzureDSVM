8a1643abaa0b3ac30f2edcf874de654084fb4077
yueguoguo <yueguoguo1024@gmail.com>
2017-06-30 15:34:06 +0800

    Added test items.

--- tests/testthat/test-7-extensions.R
+++ tests/testthat/test-7-extensions.R
@@ -46,17 +46,37 @@ deployDSVM(asc,
 context(" - Can add extension to a Ubuntu DSVM")
 
 test_that("add an extension", {
+  
+  res <- operateDSVM(asc, 
+                     resourceGroup_name, 
+                     dsvm_name,
+                     operation="Check")
+  
+  expect_identical(res, "Provisioning succeeded, VM running")
+  
   # add an extension to the DSVM.
   # script to execute for extension is from DataScienceVM github repo. It is 
   # to add new users to the DSVM and update CNTK from 2.0 to 2.0RC2.
   
-  file_url <- "https://raw.githubusercontent.com/Azure/DataScienceVM/master/Extensions/General/create-user-and-updatecntk.sh"
+  file_url <- "https://www.microsoft.com"
   command  <- "./create-user-and-updatecntk.sh"
+  
+  expect_error(addExtensionDSVM(asc,
+                                location=location,
+                                resource.group=resourceGroup_name, 
+                                hostname=dsvm_name, 
+                                os=dsvm_os,
+                                fileurl=file_url, 
+                                command=command),
+               "Extension failed.")
+  
+  file_url <- "https://raw.githubusercontent.com/Azure/DataScienceVM/master/Extensions/General/create-user-and-updatecntk.sh"
   
   res <- addExtensionDSVM(asc,
                           location=location,
                           resource.group=resourceGroup_name, 
                           hostname=dsvm_name, 
+                          os=dsvm_os,
                           fileurl=file_url, 
                           command=command)
   
@@ -72,6 +92,51 @@ test_that("check info of extension", {
   
   expect_is(res, "list")
   expect_identical(res$properties$provisioningState, "Succeeded")
+})
+
+context(" - Can add extension to a Windows DSVM")
+
+dsvm_size     <- "Standard_D4_v2"
+dsvm_os       <- "Windows"
+dsvm_name     <- paste0("dsvm", 
+                        paste(sample(letters, 3), collapse=""))
+dsvm_username <- "dsvmuser"
+dsvm_password <- "DSVM@test123"
+
+deployDSVM(asc, 
+           resource.group=resourceGroup_name,
+           location=location,
+           hostname=dsvm_name,
+           username=dsvm_username,
+           os=dsvm_os,
+           authen="Password",
+           password=dsvm_password,
+           mode="Sync")
+
+test_that("add an extension", {
+  res <- operateDSVM(asc, 
+                     resourceGroup_name, 
+                     dsvm_name,
+                     operation="Check")
+  
+  expect_identical(res, "Provisioning succeeded, VM running")
+  
+  # add an extension to the DSVM.
+  # script to execute for extension is from DataScienceVM github repo. It is 
+  # to add new users to the DSVM and update CNTK from 2.0 to 2.0RC2.
+  
+  file_url <- "https://raw.githubusercontent.com/Azure/DataScienceVM/master/Extensions/General/DowngradeMRS2Rclient.ps1"
+  command  <- "powershell.exe -ExecutionPolicy Unrestricted -DowngradeMRS2Rclient.ps1"
+  
+  res <- addExtensionDSVM(asc,
+                          location=location,
+                          resource.group=resourceGroup_name, 
+                          hostname=dsvm_name, 
+                          os=dsvm_os,
+                          fileurl=file_url, 
+                          command=command)
+  
+  expect_true(res)
 })
 
 azureDeleteResourceGroup(asc, resourceGroup=resourceGroup_name)
